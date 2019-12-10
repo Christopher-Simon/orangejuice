f66a48a961e6fa8c22d62d6e5043dbbfb5897406
Christopher Simon <christopher.simon.wells@gmail.com>
2019-12-10 13:35:57 +0100

    recovery fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 
 import 'bootstrap'
 import './src/application.scss'
+require("@fortawesome/fontawesome-free")
 require("../includes/slides.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
